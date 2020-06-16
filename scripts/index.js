cc2e4d7fe446bbfb9837966b3464e7b2dba15aea
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2020-06-16 23:18:35 +0300

    fix: исправлены замечания после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,28 @@
-let popup = document.querySelector('.popup')
-let popupOpenButton = document.querySelector('.button__open-popup')
-let popupCloseButton = popup.querySelector('.popup__close')
-let popupToggle = function (event) {
+let popup = document.querySelector('.popup');
+let popupOpenButton = document.querySelector('.button_open-popup');
+let popupCloseButton = popup.querySelector('.popup__close');
+let profile = document.querySelector('.profile');
+let formElement = document.querySelector('.form');
+let nameInput = formElement.querySelector('.form__input-container_name');
+let jobInput = formElement.querySelector('.form__input-container_job');
+let name = profile.querySelector('.profile__title');
+let job = profile.querySelector('.profile__subtitle');
+
+let popupToggle = function () {
     popup.classList.toggle('popup_opened')
+    nameInput.value = name.textContent
+    jobInput.value = job.textContent
 }
-
-popupOpenButton.addEventListener('click', popupToggle)
-popupCloseButton.addEventListener('click', popupToggle)
-
-
-let profile = document.querySelector('.profile')
-let formElement = document.querySelector('.form')
-
-let nameInput = formElement.querySelector('.form__input-container_name')
-let jobInput = formElement.querySelector('.form__input-container_job')
-let name = profile.querySelector('.profile__title')
-let job = profile.querySelector('.profile__subtitle')
-nameInput.value = 'Жак-Ив Кусто';
-jobInput.value = 'Исследователь океана';
-
-
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-
     name.textContent = nameInput.value
     job.textContent = jobInput.value
+    popupToggle();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+popupOpenButton.addEventListener('click', popupToggle);
+popupCloseButton.addEventListener('click', popupToggle);
 
-let saveAndClose = popup.querySelector('.popup__submit')
-saveAndClose.addEventListener('click', popupToggle);
 
