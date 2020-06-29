e735ce91e36dae8ed6b6e802bae9d7d733e781df
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2020-06-30 01:32:10 +0300

    feat: сделать добавление карточек через скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,52 @@ popupOpenButton.addEventListener('click', popupToggle);
 popupCloseButton.addEventListener('click', popupToggle);
 
 
+const initialCards = [
+    {
+        name: 'Архыз',
+        alt: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        alt: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        alt: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        alt: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        alt: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        alt: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const cardsListElement = document.querySelector('.elements');
+const listItem = document.createElement('li');
+const cardsTemplateElement = document.querySelector('.cards-template');
+
+
+function addCard(item) {
+    const card = cardsTemplateElement.content.cloneNode(true);
+    card.querySelector('.element__title').textContent = item.name;
+    card.querySelector('.element__image').setAttribute("src", item.link);
+    card.querySelector('.element__image').setAttribute("alt", item.alt);
+    cardsListElement.prepend(card);
+}
+
+initialCards.forEach(function (item) {
+    addCard(item);
+});
