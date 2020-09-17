64cc6e84a4f83ed21efe5cf43fdb733a1b7be495
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2020-09-17 13:24:29 +0300

    fix: исправлен баг на первом попапе

--- scripts/index.js
+++ scripts/index.js
@@ -6,17 +6,19 @@ const jobInput = formElement.querySelector('.form__input-container_job');
 const name = profile.querySelector('.profile__title');
 const job = profile.querySelector('.profile__subtitle');
 const cardsListElement = document.querySelector('.elements');
-const listItem = document.createElement('li');
 const cardsTemplateElement = document.querySelector('.cards-template');
 const formAdd = document.querySelector('.form_addCard');
-const nameInputCard = formAdd.querySelector('.form__input-container_addCard_name') // эту переменную лучше объявить сразу после addForm
-const linkInput = formAdd.querySelector('.form__input-container_addCard_link') // сделать по аналогии с linkInput
-const listItemCard = document.createElement('li');
+const nameInputCard = formAdd.querySelector('.form__input-container_addCard_name');
+const linkInput = formAdd.querySelector('.form__input-container_addCard_link');
 const popupImageImg = document.querySelector('.popup__zoom-image');
 const popupImageText = document.querySelector('.popup__zoom-title');
 
 
 const togglePopup = function (popup) {
+    if (popup.classList.contains('popup_opened') === false) {
+        nameInput.value = name.textContent;
+        jobInput.value = job.textContent;
+    }
     popup.classList.toggle('popup_opened')
 };
 
@@ -54,16 +56,13 @@ const initialCards = [
 ];
 
 //функция закрытия первого попапа (где меняем имя пользователя)
-//если попап открыт, то в инпутах показывает то же, что и в профайле, потом закрывает попап
 const toggleUserPopup = function () {
-    if (popup.classList.contains('popup_opened') === false) {
-        nameInput.value = name.textContent;
-        jobInput.value = job.textContent;
-    }
     popup.classList.toggle('popup_opened');
 }
 
-//функция: сначала отменяет стандартное поведение браузера, потом 
+//Функция сохранения изменений в первом попапе:
+//Сначала отменяет стандартное поведение браузера, потом сохраняет
+//данные, которые ввел пользователь. Потом закрывает попап. 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     name.textContent = nameInput.value;
@@ -91,13 +90,13 @@ function addCard(item) {
     //говорим, что хотим добавить карточку в начало, а не в конец
     cardsListElement.prepend(card);
 
-    const like = document.querySelector('.element__like')
+    const like = document.querySelector('.element__like');
     like.addEventListener('click', function (evt) {
-        evt.target.classList.toggle('element__like_theme_dark');
+        evt.target.classList.toggle('element__like_theme_dark')
     });
 }
 initialCards.forEach(function (item) {
-    addCard(item);
+    addCard(item)
 });
 
 //функция добавления новых карточек
@@ -132,41 +131,43 @@ const popupImageClose = popupImage.querySelector('.popup__close');
 const popupEditOpen = document.querySelector('.button_edit_open');
 const popupAddOpen = document.querySelector('.button_add_open');
 const popupImageOpen = document.querySelector('.element__image');
+
 //слушатель: при нажатии на кнопку сработает функция renderCard
 formAdd.addEventListener('submit', renderCard);
 
 popupEditOpen.addEventListener('click', function () {
     togglePopup(popupEdit)
-})
+});
 popupAddOpen.addEventListener('click', function () {
     togglePopup(popupAdd);
     nameInputCard.value = '';
     linkInput.value = '';
-})
+});
 popupEditClose.addEventListener('click', function () {
     togglePopup(popupEdit)
-})
+});
 popupAddClose.addEventListener('click', function () {
     togglePopup(popupAdd)
-})
+});
 popupImageClose.addEventListener('click', function () {
     togglePopup(popupImage)
-})
+});
 
 //функция удаления карточки
 function deleteCard(e) {
     //добавляем event на карточку, чтобы отслеживать клик по любой рандомной карточке
     const card = e.target.closest('.element');
     //когда event отследит клик, он должен  удалить карточку, по которой он был сделан
-    card.remove();
-}
+    card.remove()
+};
+
 //функция увеличения картинки в попапе
 function popupZoomImage(item) {
-    const image = item.link
-    const place = item.name
+    const image = item.link;
+    const place = item.name;
 
     popupImageImg.src = image
     popupImageText.textContent = place
 
     togglePopup(popupImage)
-}
+};
